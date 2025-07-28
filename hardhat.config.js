563a5d4d3a66f4009044d3bec6c59b553e99bdf8
PKTseng <jk0910948659@Gmail.com>
2025-07-28 11:49:44 +0800

    setting hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
+
+// url 只能放 EVM 相容鏈的 RPC 端點，例如：
+// Ethereum 主網或測試網（如 Sepolia、Goerli）
+// Polygon、BSC、Arbitrum、Optimism 等 EVM 相容鏈
+// 這些 RPC 端點通常來自 Infura、Alchemy、Chainstack 或官方節點
+// 不能放 Solana、Aptos、Sui 等非 EVM 鏈的 RPC。
+
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
