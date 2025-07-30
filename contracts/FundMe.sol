72ba0a4b3e2517225c7c42315e0497e80860fd79
PKTseng <jk0910948659@Gmail.com>
2025-07-30 16:54:07 +0800

    update contract explain

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,10 +4,16 @@ pragma solidity ^0.8.20;
 // import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-// 1. 创建一个收款函数
-// 2. 记录投资人并且查看
-// 3. 在锁定期内，达到目标值，生产商可以提款
-// 4. 在锁定期内，没有达到目标值，投资人在锁定期以后退款
+// 業務邏輯
+// 這個合約實現了一個有時間限制的眾籌系統：
+
+// 眾籌目標：募集價值 1000 美元的 ETH
+// 最低投資額：每次投資至少價值 100 美元的 ETH
+// 時間窗口：有一個鎖定期（lockTime），在此期間可以投資
+
+// 成功條件：
+// 如果在鎖定期內達到目標金額，項目方（owner）可以提取所有資金
+// 如果在鎖定期內未達到目標，投資人可在鎖定期結束後申請退款
 
 contract FundMe {
   mapping(address => uint256) public fundersToAmount;
