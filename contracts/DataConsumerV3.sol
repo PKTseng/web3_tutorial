944f407df42eb53d2b68dd4bf575299c193a3bed
PKTseng <jk0910948659@Gmail.com>
2025-08-04 13:58:55 +0800

    update deploy setting

--- contracts/DataConsumerV3.sol
+++ contracts/DataConsumerV3.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-contract FundMe {
+contract DataConsumerV3 {
   AggregatorV3Interface internal dataFeed;
 
   mapping(address => uint256) public foundersWithAmount;
