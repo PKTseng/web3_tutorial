52f43de363a632e6107e8c6c79a9b5534ff50875
PKTseng <jk0910948659@gmail.com>
2025-08-09 11:21:05 +0800

    add common js

--- deploy/00-deploy-fund-me.js
+++ deploy/00-deploy-fund-me.js
@@ -1,10 +1,12 @@
+const { DECIMAL, INITIAL_ANSWER } = require("../helper-hardhat.config");
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { firstAccount } = await getNamedAccounts();
   const { deploy } = deployments;
 
   await deploy("MockV3Aggregator", {
     from: firstAccount,
-    args: [8, 300000000000],
+    args: [DECIMAL, INITIAL_ANSWER],
     log: true,
   });
 };
