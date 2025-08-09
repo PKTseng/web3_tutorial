bb208010db0dbae516e25531af0857a703552b05
PKTseng <jk0910948659@gmail.com>
2025-08-09 13:24:20 +0800

    add config constant

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,21 +1,28 @@
-// module.exports = (hre) => {
-//   console.log("deploy function", hre);
-// };
+const { network } = require("hardhat");
+const { DEVELOPMENT_CHAINS, NETWORK_CONFIG, LOCK_TIME } = require("../helper-hardhat.config");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  // console.log("deployments", getNamedAccounts);
-  // console.log("deployments", deployments);
-
   const { firstAccount } = await getNamedAccounts();
-  const { deploy, log } = deployments;
+  const { deploy, log, get } = deployments;
 
   log("----------------------------------------------------");
   log("正在部署 FundMe 合約...");
   log("部署帳戶:", firstAccount);
+  // log("network", network); // 查看 network 詳細資訊
+
+  let dataFeedAddr;
+  if (DEVELOPMENT_CHAINS.includes(network.name)) {
+    const mockV3Aggregator = await get("MockV3Aggregator");
+    dataFeedAddr = mockV3Aggregator.address;
+  } else {
+    dataFeedAddr = NETWORK_CONFIG[network.config.chainId].ethUsdDatafeed;
+  }
+
+  log("mockDataFeed 地址 :", dataFeedAddr);
 
   const fundMe = await deploy("FundMe", {
     from: firstAccount,
-    args: [180],
+    args: [LOCK_TIME, dataFeedAddr],
     log: true,
   });
 
