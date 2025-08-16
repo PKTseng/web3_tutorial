e666b8ded9d5f45b9cbb4e7f2818a89fbee17fc8
PKTseng <jk0910948659@gmail.com>
2025-08-16 10:44:30 +0800

    完成基本合約

--- contracts/ReadWriteTransfer.sol
+++ contracts/ReadWriteTransfer.sol
@@ -2,21 +2,41 @@
 pragma solidity ^0.8.30;
 
 contract ReadWriteTransfer {
-  address public owner;
-  uint256 public number;
+  mapping(address => uint256) public balances;
 
-  bool private _lock;
-
-  constructor(uint256 _number) {
-    owner = msg.sender;
-    number = _number;
+  function getBalance(address _address) public view returns (uint256) {
+    return balances[_address];
   }
 
-  function getNumber() external view returns (uint256) {
-    return number;
+  function deposit() public payable {
+    require(msg.value > 0, "Need more ETH");
+
+    balances[msg.sender] += msg.value;
   }
 
-  function setNumber(uint256 _number) external {
-    number = _number;
+  function transfer(address payable _to, uint256 _amount) public {
+    require(_to != address(0), "Invalid recipient address");
+    require(balances[_to] > 0, "No balance");
+
+    balances[msg.sender] -= _amount;
+    balances[_to] += _amount;
+  }
+
+  function checkBalanceAndTransfer(
+    address payable _to,
+    uint256 _amount
+  ) public {
+    uint256 currentBalance = getBalance(msg.sender);
+    require(currentBalance > _amount, "Insufficient amount");
+
+    transfer(_to, currentBalance);
+  }
+
+  function withdraw(uint256 _amount) public {
+    require(balances[msg.sender] > _amount, "In");
+    balances[msg.sender] -= _amount;
+
+    (bool success, ) = payable(msg.sender).call{ value: _amount }("");
+    require(success, "Transfer fail");
   }
 }
